a94341d6bd583f2aefa582b15fdbc88e483de232
Melih aus Beeck <melihesad@hotmail.de>
2023-11-16 00:22:42 +0100

    Projektdateien hinzufügen.

---
+++ Core/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Gib eine Formel ein:");
+        string input = Console.ReadLine();
+
+        try
+        {
+            double result = RemoveSpaces(input);
+            Console.WriteLine($"Ergebnis: {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fehler: {ex.Message}");
+        }
+    }
+}
+
+  
